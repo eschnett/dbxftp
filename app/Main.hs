2ebafb1ac1d13c496957fca7af8ca2ff3dcb976b
Erik Schnetter <schnetter@gmail.com>
2020-03-09 14:19:08 -0400

    Update location for example data

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,7 @@ import qualified DBXFTP
 
 main :: IO ()
 main =
-  do appState <- DBXFTP.newAppState
-     DBXFTP.put appState ["/Users/eschnett/src/hs/dbxftp/data"] "/test-20200307"
+  do putStrLn "DBXFTP"
+     appState <- DBXFTP.newAppState
+     DBXFTP.put appState ["data"] "/test-20200309"
+     putStrLn "Done."
