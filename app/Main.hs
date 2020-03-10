54281d559c04b2fd5cb71b99501755cc80dd386c
Erik Schnetter <schnetter@gmail.com>
2020-03-10 10:23:36 -0400

    Update test data directory

--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,5 @@ main =
   do putStrLn "DBXFTP"
      appState <- DBXFTP.newAppState
      -- DBXFTP.ls appState "/test-20200309"
-     DBXFTP.put appState ["/tmp/data"] "/test-20200309"
+     DBXFTP.put appState ["/tmp/eschnett/data"] "/test-20200309"
      putStrLn "Done."
